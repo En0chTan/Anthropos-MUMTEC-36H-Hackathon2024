a50d164f044a23d890a493fc91eff55b4cd6605f
Chee Rui <111403250+ARandomRui@users.noreply.github.com>
2024-09-28 16:50:18 +0800

    Update backend_http.py

--- backend_http.py
+++ backend_http.py
@@ -5,6 +5,7 @@ import test
 import json
 import sys
 import os
+import re
 app = Flask(__name__)
 CORS(app)
 
@@ -20,53 +21,58 @@ def post_api_data():
     data = request.get_json()
     code = data.get("code")
     flags = data.get("flags")
-    
-    option_map={"1":"turnOffSerial","2":"optimizeDPA","3":"optimizePeriodicTask","4":"optimize_fp_math"}
-    options = {"turnOffSerial":False,"optimizeDPA":False,"optimizePeriodicTask":False,"optimize_fp_math":False}
-    for option, value in flags.items():
-        print(option_map[option])
-        print(options[option_map[option]])
-        print(value)
-        options[option_map[option]] = value
-        
-    print(code)
+          
 
     prompt = prompts.REVIEW_PROMPT + prompts.CODE_BEGINS + code + prompts.CODE_ENDS
 
     # Review the code so that it produces a json output to check if the code can be optimized or not
-    review = json.loads(test.AIprompting(prompt))
-    print(review['Arduino_Code'])
+    options = json.loads(test.AIprompting(prompt))
+    used_flags = list()
+    for index,each in enumerate(options.values()):
+        print(index)
+        print(each)
+        if index != 0:
+            used_flags.append(1 if each == "True" else 0) 
+    print(str(used_flags))
 
     #Turn off program if Not Arduino Code
-    if review['Arduino_Code'] == 'Yes':
+    if options['Arduino_Code'] == "True":
         pass
     else: 
         return jsonify({"error": "Invalid code"})
         
-    if options["turnOffSerial"] == True:
+    if options["optimizeSerial"] == "True":
         code = test.remove_serial_print(code)
         
-    if options["optimizeDPA"] == True:
+    if options["optimizeDPA"] == "True":
         add_3_count = False
         if "#define SET_PIN_HIGH(port, pin) (PORT ## port |= (1 << pin))" not in code:
             add_3_count = True
         code_dict = {}
         code_dict = test.find_all_read_write(code)
-        print("erererererererer\n" + str(code_dict) + "\nasdasdasdasdasdasdas")
         if code_dict:
             code = test.Add_DPA_header(code)
             code = test.Read_Write_AI_Optimization(code,code_dict,prompts,add_3_count)
+        print(code)
+    
+    if options["optimizePeriodicTask"] == "True":
+        periodic_task_prompt = prompts.PERIODIC_TASK_PROMPT + prompts.CODE_BEGINS + code + prompts.CODE_ENDS
+        code = test.AIprompting(periodic_task_prompt)
 
-    if options["optimize_fp_math"] == True:
+
+    if options["optimize_fp_math"] == "True":
         analyze_result = prompts.ANALYZE_FP_MATH +prompts.CODE_BEGINS + prompts.FP_MATH_MACRO  + code + prompts.CODE_ENDS
         analyze_result = test.AIprompting(analyze_result)
         fp_math_prompt = prompts.INT_MATH_PROMPT + analyze_result + prompts.CODE_BEGINS + prompts.FP_MATH_MACRO  + code + prompts.CODE_ENDS
         code = test.AIprompting(fp_math_prompt)
-    return jsonify({"optimized_code":code}), 201
-    
+    code = code.split("\n")
+    if re.search(r'^\s*```c\s*$',code[0]):
+        code.pop(0)
+    if re.search(r'^\s*```\s*$',code[-1]):
+        code.pop(-1)
+    code = "\n".join(code)
+    return jsonify({"optimized_code":code, "used_flags":used_flags }), 201
 
-
-    
     if code is None:
         return jsonify({"error": "Missing 'code' in request data"}), 400
     return jsonify({"received_code": code, "received_flags": flags}), 201
